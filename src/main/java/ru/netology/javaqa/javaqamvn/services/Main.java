733089be259c576fd804230a8e371c571640044a
kris-i-kris <flare120491@gmail.com>
2023-08-14 16:40:51 +0700

    Initial commit

---
+++ src/main/java/ru/netology/javaqa/javaqamvn/services/Main.java
@@ -0,0 +1,11 @@
+package ru.netology.javaqa.javaqamvn.services;
+
+public class Main {
+    public static void main(String[] args) {
+        freelanceTest service = new freelanceTest();
+    }
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/freelanceTest.java
@@ -0,0 +1,7 @@
+package ru.netology.javaqa.javaqamvn.services;
+public class freelanceTest {
+    public  int calculate(int income, int expenses) {
+        int income = 10_000;
+        int expenses = 3_000;
+    }
+}
