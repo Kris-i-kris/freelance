cf38c4d013b3a7b32b64ee862f6f20362bc560fb
kris-i-kris <flare120491@gmail.com>
2023-08-14 22:41:56 +0700

    Initial commit

--- src/main/java/ru/netology/javaqa/javaqamvn/services/Main.java
+++ src/main/java/ru/netology/javaqa/javaqamvn/services/Main.java
@@ -2,10 +2,14 @@ package ru.netology.javaqa.javaqamvn.services;
 
 public class Main {
     public static void main(String[] args) {
-        freelanceTest service = new freelanceTest();
-    }
-    }
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        freelanceService service = new freelanceService();
+
+        int expected = 3;
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        System.out.println("Колличество месяцев для отдыха: " + " Ожидаемый " + expected + "; Реальный " + actual);
+
+        expected = 2;
+        actual = service.calculate(100_000, 60_000, 150_000);
+        System.out.println("Колличество месяцев для отдыха: " + " Ожидаемый " + expected + "; Реальный " + actual);
     }
 }
--- src/test/java/ru/netology/javaqa/javaqamvn/services/freelanceTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/freelanceTest.java
@@ -1,7 +1,36 @@
 package ru.netology.javaqa.javaqamvn.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 public class freelanceTest {
-    public  int calculate(int income, int expenses) {
+
+    @Test
+    void restThreeMonths() {
+        freelanceService service = new freelanceService();
+
         int income = 10_000;
         int expenses = 3_000;
+        int threshold = 20_000;
+        int expected = 3;
+
+        long actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    void restThoMonths() {
+        freelanceService service = new freelanceService();
+
+        int income = 100_000;
+        int expenses = 60_000;
+        int threshold = 150_000;
+        int expected = 2;
+
+        long actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
