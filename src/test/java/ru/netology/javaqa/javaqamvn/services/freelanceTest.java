55d30913d60e99cfebb19fdf8b219238721d9e00
kris-i-kris <flare120491@gmail.com>
2023-08-17 12:29:56 +0700

    csv

--- src/test/java/ru/netology/javaqa/javaqamvn/services/freelanceTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/freelanceTest.java
@@ -1,37 +1,38 @@
 package ru.netology.javaqa.javaqamvn.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class freelanceTest {
 
-    @Test
-    void restThreeMonths() {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/data.csv")
+    public void restThreeMonths(int expected, int income, int expenses, int threshold) {
         freelanceService service = new freelanceService();
 
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
-
-        long actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-
-
-    }
-
-    @Test
-    void restThoMonths() {
-        freelanceService service = new freelanceService();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
+        //int income = 10_000;
+        //int expenses = 3_000;
+        //int threshold = 20_000;
+        //int expected = 3;
 
         long actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
+
+    //@Test
+    //void restThoMonths() {
+    //freelanceService service = new freelanceService();
+
+    //int income = 100_000;
+    //int expenses = 60_000;
+    //int threshold = 150_000;
+    //int expected = 2;
+
+    //long actual = service.calculate(income, expenses, threshold);
+
+    //Assertions.assertEquals(expected, actual);
+    //}
 }
